308e8da1f5123579caab250e36f3f2d5fdca5323
faun3 <antonioculcea@gmail.com>
2023-10-29 16:12:01 +0200

    start work on image gallery

---
+++ src/components/ImageGallery.svelte
@@ -0,0 +1,11 @@
+<script>
+  import SlidingImage from "./SlidingImage.svelte";
+</script>
+
+<div class="w-full flex justify-center items-center bg-stone">
+  <div class="z-[999] w-fit">
+    <SlidingImage src="/images/galleryImg1.png" />
+    <SlidingImage src="/images/galleryImg2.png" />
+    <SlidingImage src="/images/galleryImg3.png" />
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import Header from "../components/Header.svelte";
   import Namedrop from "../components/Namedrop.svelte";
+  import ImageGallery from "../components/ImageGallery.svelte";
 </script>
 
 <Header />
 <Namedrop />
+<ImageGallery />
