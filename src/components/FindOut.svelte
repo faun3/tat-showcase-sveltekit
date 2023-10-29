b50a600ae1bf189d18eae401ab68c440c8723333
faun3 <antonioculcea@gmail.com>
2023-10-29 17:40:51 +0200

    start work on last website component

---
+++ src/components/FindOut.svelte
@@ -0,0 +1,13 @@
+<div class="bg-stone w-full flex justify-center">
+  <div class="w-[90rem] h-[75rem] overflow-hidden relative">
+    <div
+      class="font-garamond text-[4rem] font-regular uppercase absolute left-[4rem] bottom-[calc(100%/6)]"
+    >
+      <p>find</p>
+      <p>out</p>
+    </div>
+    <a href="https://google.com" class="absolute left-1/2">
+      <img src="/images/gateImage.png" alt="" class="mix-darken" /></a
+    >
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
   import Header from "../components/Header.svelte";
   import Namedrop from "../components/Namedrop.svelte";
   import ImageGallery from "../components/ImageGallery.svelte";
+  import FindOut from "../components/FindOut.svelte";
 </script>
 
 <Header />
 <ImageGallery />
 <Namedrop />
+<FindOut />
