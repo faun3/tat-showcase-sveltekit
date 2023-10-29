73e5a719eab7a98c0474799f326c0ca38575e6d6
faun3 <antonioculcea@gmail.com>
2023-10-29 17:52:28 +0200

    update readme and link to deployment

--- src/components/FindOut.svelte
+++ src/components/FindOut.svelte
@@ -1,13 +1,16 @@
 <div class="bg-stone w-full flex justify-center">
-  <div class="w-[90rem] h-[75rem] overflow-hidden relative">
-    <div
+  <div class="w-[90rem] h-[75rem] relative">
+    <a
       class="font-garamond text-[4rem] font-regular uppercase absolute left-[4rem] bottom-[calc(100%/6)]"
+      href="https://github.com/faun3/tat-showcase-sveltekit"
     >
       <p>find</p>
       <p>out</p>
-    </div>
-    <a href="https://google.com" class="absolute left-1/2">
-      <img src="/images/gateImage.png" alt="" class="mix-darken" /></a
-    >
+    </a>
+    <img
+      src="/images/gateImage.png"
+      alt=""
+      class="mix-darken h-[110%] absolute left-1/2"
+    />
   </div>
 </div>
